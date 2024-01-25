b8189271265863157fa101b6320dbea7c9117dd8
ElieAssaf33 <you@example.com>
2024-01-25 14:08:43 +0200

    liko_nuslifuot

--- escape_game.py
+++ escape_game.py
@@ -1,94 +1,107 @@
 import PySimpleGUI as sg
+from layouts import Layouts
 
+level_one = sg.Window('Escape: PIRMAS LYGIS', Layouts.layout_one(), font='Arial 20')
 
-layout_one = [  
-    [sg.Text('PIRMAS LYGIS')],
-    [sg.Text('Pabandykite išeiti iš programos')],
-    [sg.Text('Turėkite omenyje: programa apgaulinga')],
-    [sg.Text('Ar tikrai norite išeiti iš programos?')],
-    [sg.Text('Įveskite "Ne" arba "Taip"')],
-    [sg.Text('Įvedę pasirinkimą paspauskite patvirtinimo mygtuką')],
-    [sg.Input(key='INPUT_ONE')],
-    [
-    sg.Button('Patvirtinti pasirinkimą "Taip"', key='YES'),
-    sg.Button('Patvirtinti pasirinkimą "Ne"', key='NO'),
-    sg.Button('Tęsti', key='CONTINUE_ONE', visible=False)
-    ],
-    [sg.Text(key='OUTPUT_ONE')]
-]
+level_two = sg.Window('Escape: ANTRAS LYGIS', Layouts.layout_two(), font='Arial 20')
 
-layout_two = [
-    [sg.Text('ANTRAS LYGIS')],
-    [sg.Text('?somargorp ši itieši etiron radsiv rA')],
-    [sg.Text('"eN" abra "piaT" etiksevĮ')],
-    [sg.Text('ąkutgym ominitrivtap etiksuapsap ąmiknirisap ędevĮ')],
-    [sg.Input(key='INPUT_TWO')],
-    [
-    sg.Button('"piaT" ąmiknirisap itnitrivtaP', key='YES'),
-    sg.Button('"eN" ąmiknirisap itnitrivtaP', key='NO'),
-    sg.Button('itsęT', key='CONTINUE_TWO', visible=False)
-    ],
-    [sg.Text(key='OUTPUT_TWO')]
-]
+level_three = sg.Window('Escape: III LYGIS', Layouts.layout_three(), font='Arial 20')
 
-layout_three = [
-    [sg.Text('III LYGIS')],
-    [sg.Text('Sveikiname, pasiekėte III lygį!')],
-    [sg.Text('Atspėkite skaičių nuo 1 iki 10!')],
-    [sg.Text('Įvedę spėjimą paspauskite spėjimo mygtuką')],
-    [sg.Input(key='INPUT_THREE')],
-    [
-    sg.Button('Spėti', key='GUESS')
-    ],
-    [sg.Text(key='OUTPUT_THREE')]
-]
+level_four = sg.Window('Escape: KETVIRTAS LYGIS - NEMATOMAS RAŠALAS', Layouts.layout_four(), font='Arial 20')
 
-level_one = sg.Window('Escape: PIRMAS LYGIS', layout_one, font='Arial 20')
+level_five = sg.Window('Escape: 5 lygis', Layouts.layout_five(), font ='Arial 20', right_click_menu=sg.MENU_RIGHT_CLICK_EXIT)
 
-level_two = sg.Window('Escape: ANTRAS LYGIS', layout_two, font='Arial 20')
-
-level_three = sg.Window('Escape: III LYGIS', layout_three, font='Arial 20')
-
+game_over = sg.Window('Game Over', Layouts.layout_game_over(), font = 'Arial 20')
 
 if level_one:
     while True:
         event, values = level_one.read()
         if event == sg.WINDOW_CLOSED:
             break
-        elif event == 'YES' and values['INPUT_ONE'] == 'Taip':
-            level_one['OUTPUT_ONE'].update(f'Pasirinkimas "{values["INPUT_ONE"]}" neteisingas!')
-        elif event == 'NO' and values['INPUT_ONE'] == 'Ne':
-            level_one['OUTPUT_ONE'].update(f'Pasirinkimas "{values["INPUT_ONE"]}" neteisingas')
-        elif event == 'YES' and values['INPUT_ONE'] == 'Ne':
-            level_one['OUTPUT_ONE'].update('Pasirinkimas teisingas, spauskite mygtuką "Tęsti"')
-            level_one['CONTINUE_ONE'](visible=True)
-        elif event == 'NO' and values['INPUT_ONE'] == 'Taip':
-            level_one['OUTPUT_ONE'].update('Pasirinkimas teisingas, spauskite mygtuką "Tęsti"')
+        elif event == 'Exit' :
+            level_one['OUTPUT_ONE'].update('Nope')
+        elif event == 'patvirtinti' and not values['INPUT_ONE'] == '"Ne" arba "Taip"':
+            level_one['OUTPUT_ONE'].update('Pasirinkimas neteisingas, bandykite dar karta')
+        elif event == 'patvirtinti' and values['INPUT_ONE'] == '"Ne" arba "Taip"':
+            level_one['OUTPUT_ONE'].update(f'Pasirinkimas teisingas!')
             level_one['CONTINUE_ONE'](visible=True)
         if event == 'CONTINUE_ONE':
             level_one.close()
-            level_two.read()          
-            
+            level_two.read()
 
 if level_two:
     while True:
-        event_2, values_2 = level_two.read()        
-        if event_2 == sg.WIN_CLOSED:
+        event_two, values_two = level_two.read()    
+        if event_two == sg.WINDOW_CLOSED:
             break
-        elif event_2 == 'YES' and values_2['INPUT_TWO'] == 'eN':
-            level_two['OUTPUT_TWO'].update(f'sagnisieten "{values_2["INPUT_TWO"]}" samiknirisaP')
-        elif event_2 == 'NO' and values_2['INPUT_TWO'] == 'piaT':
-            level_two['OUTPUT_TWO'].update(f'sagnisieten "{values_2["INPUT_TWO"]}" samiknirisaP')
-        elif event_2 == 'YES' and values_2['INPUT_TWO'] == 'piaT':
-            level_two['OUTPUT_TWO'].update('"itsęT" ąkutgym etiksuaps, sagnisiet samiknirisaP')
+        elif event_two == 'Exit':
+            level_two['OUTPUT_TWO'].update('Nope')
+        elif event_two == 'patvirtinti_two' and values_two['INPUT_TWO'] == "piaT" or values_two['INPUT_TWO'] == "eN":
+            level_two['OUTPUT_TWO'].update(f"Pasirinkimas teisingas!")
             level_two['CONTINUE_TWO'](visible=True)
-        elif event_2 == 'NO' and values_2['INPUT_TWO'] == 'eN':
-            level_two['OUTPUT_TWO'].update('"itsęT" ąkutgym etiksuaps, sagnisiet samiknirisaP')
-            level_two['CONTINUE_TWO'](visible=True)
-        if event_2 == 'CONTINUE_TWO':
-            level_two.close()
+        elif event_two == 'patvirtinti_two' and not values_two['INPUT_TWO'] == "piaT" or not values_two['INPUT_TWO'] == "eN":
+            level_two['OUTPUT_TWO'].update("Pasirinikinkimas neteisingas, bandykite dar karta")
+        if event_two == 'CONTINUE_TWO':
             level_three.read()
+            level_two.close()
 
-level_one.close()
-level_two.close()
-level_three.close()
+if level_three:
+
+    while True:
+        event_3, values_3 = level_three.read()
+        atsakymas = 'III'
+        teisingas_skaicius = '3'
+        teisingi_stringai =  ['trys', 'three', 'tres', 'trois', 'drei', 'tri']
+        neteisingi_skaiciai = ['1', '2', '4', '5', '6', '7', '8', '9', '10']
+        if event_3 == sg.WINDOW_CLOSED:
+            break 
+        elif event_3 == 'GUESS' and values_3['INPUT_THREE'] in neteisingi_skaiciai:
+            level_three['OUTPUT_THREE'].update('Neteisingas skaičiaus formatas, bandykite dar kartą')
+        elif event_3 == 'GUESS' and values_3['INPUT_THREE'] == teisingas_skaicius:
+            level_three['OUTPUT_THREE'].update('Skaičių atspėjote, tačiau neteisingas skaičiaus formatas,\nbandykite dar kartą')
+        elif event_3 == 'GUESS' and values_3['INPUT_THREE'] in teisingi_stringai:
+            level_three['OUTPUT_THREE'].update('Skaičių atspėjote, teisingas skaičiaus formatas,\npabandykite įvesti skaičių senovine garsios imperijos kalba')
+        elif event_3 == 'GUESS' and values_3['INPUT_THREE'] != atsakymas:
+            level_three['OUTPUT_THREE'].update('Teisingas skaičiaus formatas, skaičiaus neatspėjote, bandykite dar kartą')
+        elif event_3 == 'GUESS' and values_3['INPUT_THREE'] == atsakymas:
+            level_three['OUTPUT_THREE'].update('Pasirinkimas teisingas, spauskite mygtuką "Tęsti"')
+            level_three['CONTINUE_THREE'](visible=True)
+        if event_3 == 'CONTINUE_THREE':
+            level_four.read()
+            level_three.close()
+    
+
+if level_four:
+    while True:
+        event_4, values_4 = level_four.read()
+        if event_4 == sg.WINDOW_CLOSED:
+            break
+        elif event_4 == 'MYGTUKAS' and values_4['INPUT_FOUR'] != '1337':
+            level_four['OUTPUT_FOUR'].update('Užduotis neįvykdyta, bandykite dar kartą')
+        elif event_4 == 'MYGTUKAS' and values_4['INPUT_FOUR'] == '1337':
+            level_four['OUTPUT_FOUR'].update('Užduotis įvykdyta, spauskite mygtuką "Tęsti"')
+            level_four['CONTINUE_FOUR'](visible=True)
+            level_four.close()
+            level_five()
+
+
+if level_five:
+
+    while True:
+        event_5, values_5 = level_five.read()
+        if event_5 == sg.WINDOW_CLOSED:
+            break
+        elif event_5 == 'Exit?':
+            level_five['OUTPUT_FIVE'].update('Nope')
+        elif event_5 == 'Exit':
+            level_five['OUTPUT_FIVE'].update('Game completed')
+
+if game_over:
+    
+    while True:
+        event, value = game_over.read()
+        if event_5 == sg.WINDOW_CLOSED:
+            break
+
+
+
